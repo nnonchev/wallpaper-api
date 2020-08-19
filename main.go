f7db864a4d4f753ccb482828a4dea822bb2efea9
Nikola Nonchev <nnonchev@protonmail.com>
2020-08-19 21:12:42 +0200

    Added netClient

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"net/http"
 )
 
+func newClient() *http.Client {
+	netClient := new(http.Client)
+	netClient.Timeout = time.Second * 10
+
+	return netClient
+}
+
 func main() {
-	fmt.Println("Hello world")
+	netClient := newClient()
 }
