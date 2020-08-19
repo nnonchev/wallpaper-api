8216ef77a24f7e4c9b9bb53e0b8015d6a3ff23a6
Nikola Nonchev <nnonchev@protonmail.com>
2020-08-19 21:14:23 +0200

    Added Wallpaper && Wallpapers struct

--- main.go
+++ main.go
@@ -6,6 +6,16 @@ import (
 	"net/http"
 )
 
+type Wallpaper struct {
+	ID         string `json:"id"`
+	Path       string `json:"path"`
+	Resolution string `json:"resolution"`
+  }
+  
+  type Wallpapers struct {
+	Data []Wallpaper `json:"data"`
+  }
+
 func newClient() *http.Client {
 	netClient := new(http.Client)
 	netClient.Timeout = time.Second * 10
@@ -15,4 +25,7 @@ func newClient() *http.Client {
 
 func main() {
 	netClient := newClient()
+	s := new(Wallpapers)
+
+	fmt.Println(netClient, s)
 }
